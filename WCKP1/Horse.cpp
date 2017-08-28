2998c6150e82f152dd229c984682ee6845f9680e
Denews <gkvms963@naver.com>
2017-08-28 19:13:36 +0900

    TLevel삭제

--- WCKP1/Horse.cpp
+++ WCKP1/Horse.cpp
@@ -2,7 +2,7 @@
 #include "Input.h"
 
 Horse::Horse(ID3D11Device* device) :
-	GameObject(device, 82, 442, 0, &m_Sprite),
+	GameObject(device, 41, 221, 0, &m_Sprite),
 	m_Sprite(2, 5, { "Horse0", "Horse1" }),
 	m_IsMoving(false),
 	m_Direction(XM_PIDIV2),
